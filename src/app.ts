a4f5bae8573585d897297f2ec72ba3d67bc82683
Barend Erasmus <developersworkspace@gmail.com>
2017-04-24 13:24:47 +0200

    update

--- src/app.ts
+++ src/app.ts
@@ -82,7 +82,7 @@ export class FeatureToggleApi {
     }
 }
 
-const port = 3000;
+const port = process.env.PORT | 3000;
 
 FeatureToggleApi.repositoryFactory = new RepositoryFactory();
 const api = new FeatureToggleApi(express(), port);
